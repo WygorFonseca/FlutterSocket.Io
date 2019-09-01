29da921e8476982c73d9e8a120ed1cd198bdcfda
Wygor Fonseca <wygorfonseca@hotmail.com>
2019-09-01 20:03:35 -0300

    Funcionalidade de conexão com socket.io adicionada

--- lib/flutter_socket_io.dart
+++ lib/flutter_socket_io.dart
@@ -5,3 +5,11 @@ class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
 }
+
+/// Socket Instance
+class SocketIo {
+  /// Initialize socket connection
+  List<dynamic> connect(String hostname, int port) {
+    return [hostname, port];
+  }
+}
